e24ce7670675d0561aac0271019f952cd750f652
johnpiapian <johnpiapian@gmail.com>
2021-01-30 23:41:53 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
-String.prototype.isEmpty = function() {
+String.prototype.isEmpty = function () {
     return (this.length === 0 || !this.trim());
 };
 
-function sendEmail(_name, _email, _message) { 
-    Email.send({
-        SecureToken : "<your generated token>",
-        To : 'recipient@example.com',
-        From : "sender@example.com",
-        Subject : "Test Email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-    }).then(
-        message => alert("mail sent successfully")
-    );    
-} 
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
 
-function handleFormSubmission(e){
-
+function handleFormSubmission(e) {
     let nameEl = e.target.name;
     let emailEl = e.target.email;
     let messageEl = e.target.message;
 
-    if(!nameEl.value.isEmpty() && !emailEl.value.isEmpty() && !messageEl.value.isEmpty()){
-        // window.atob('am9obnBpYXBpYW5AZ21haWwuY29t')
-        sendEmail(nameEl.value, emailEl.value, messageEl.value);
-    }else{
-        console.log("Invalid form!");
+    if (!nameEl.value.isEmpty() && !emailEl.value.isEmpty() && !messageEl.value.isEmpty()) {
+        var data = new FormData(e.target);
+        ajax(form.method, form.action, data, success, error);
+    } else {
+        alert("Invalid input!");
     }
 
     return false;
 }
 
-function app(){
-    let formEl = document.getElementById('sendmessage');
-
-    formEl.onsubmit = handleFormSubmission;
+function app() {
+    document.getElementById('sendmessage').onsubmit = handleFormSubmission;
 }
-
 
 app();
